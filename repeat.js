62da40d7df32ef276780154d058bb4f7a88d2dae
Timothy Chang <superfluous11@gmail.com>
2018-08-21 09:47:31 -0700

    completed repeat function

---
+++ repeat.js
@@ -0,0 +1,16 @@
+function repeat(fn, n){
+    for(let i = 0; i < n; i++){
+        fn();
+    }
+}
+
+function hello(){
+    console.log("Hello world");
+}
+
+function goodbye(){
+    console.log("Goodbye world");
+}
+
+repeat(hello, 5);
+repeat(goodbye, 5);
